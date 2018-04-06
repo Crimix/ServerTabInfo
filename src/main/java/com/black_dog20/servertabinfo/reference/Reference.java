2d8bf0e27b4a63b54fb069db02ed8e3c53a8df88
Crimix <christofferm@live.dk>
2018-04-06 18:32:51 +0200

    - Refixed after a merge from master

--- src/main/java/com/black_dog20/servertabinfo/reference/Reference.java
+++ src/main/java/com/black_dog20/servertabinfo/reference/Reference.java
@@ -5,7 +5,7 @@ public class Reference {
 	public static final String MOD_ID = "servertabinfo";
 	public static final String MOD_NAME = "Server Tab Info";
 	public static final String VERSION = "@VERSION@";
-	public static final String MC_VERSIONS = "[1.12,1.12.2]";
+	public static final String MC_VERSIONS = "[1.11,1.11.2]";
 	public static final String DEPENDENCIES = "";
 	public static final String CLIENT_PROXY_CLASS = "com.black_dog20.servertabinfo.proxies.ClientProxy";
 	public static final String SERVER_PROXY_CLASS = "com.black_dog20.servertabinfo.proxies.ServerProxy";
