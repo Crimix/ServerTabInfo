792afeb89af32158d6dd8f7c91ea93521d3621e8
Crimix <christofferm@live.dk>
2018-06-17 15:20:50 +0200

    - Tried to make it more easy to develop for different minecraft versions  - Resolves #2 by adding a way to replace the normal player tab list

---
+++ src/main/java/com/black_dog20/servertabinfo/utility/CompatibilityHelper.java
@@ -0,0 +1,66 @@
+package com.black_dog20.servertabinfo.utility;
+
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.util.text.TextComponentString;
+import net.minecraft.util.text.TextComponentTranslation;
+import net.minecraft.util.text.TextFormatting;
+
+public class CompatibilityHelper {
+	
+	public static String translate(String text) {
+		return new TextComponentTranslation(text).getFormattedText();
+	}
+	
+	public static String translate(String text, ColorObject color) {
+		TextComponentTranslation temp = new TextComponentTranslation(text);
+		temp.getStyle().setColor(color.getColor());
+		return temp.getFormattedText();
+	}
+	
+	public static String text(String text, ColorObject color) {
+		TextComponentString temp = new TextComponentString(text);
+		temp.getStyle().setColor(color.getColor());
+		return temp.getFormattedText();
+	}
+	
+	public static String text(String text) {
+		TextComponentString temp = new TextComponentString(text);
+		return temp.getFormattedText();
+	}
+	
+	public static void glPush() {
+		GlStateManager.pushMatrix();
+	}
+	
+	public static void glPop() {
+		GlStateManager.popMatrix();
+	}
+	
+	public static void glScale(double value) {
+		GlStateManager.scale(value, value, value);
+	}
+	
+	public static void glListHelper() {
+		GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+		GlStateManager.enableAlpha();
+		GlStateManager.enableBlend();
+		GlStateManager.tryBlendFuncSeparate(GlStateManager.SourceFactor.SRC_ALPHA, GlStateManager.DestFactor.ONE_MINUS_SRC_ALPHA, GlStateManager.SourceFactor.ONE, GlStateManager.DestFactor.ZERO);
+	}
+	
+	public static int getStringWidth(Minecraft mc, String text) {
+		return mc.fontRenderer.getStringWidth(text);
+	}
+	
+	public static void drawStringWithShadow(Minecraft mc, String text, float x, float y, int color) {
+		mc.fontRenderer.drawStringWithShadow(text, x, y, color);
+	}
+	
+	public static void drawStringWithShadowItalic(Minecraft mc, String text, float x, float y, int color) {
+		mc.fontRenderer.drawStringWithShadow(TextFormatting.ITALIC + text, x, y, color);
+	}
+	
+	public static int getFontRenderHeight(Minecraft mc) {
+		return mc.fontRenderer.FONT_HEIGHT;
+	}
+}
---
+++ src/main/java/com/black_dog20/servertabinfo/utility/PlayerInfo.java
@@ -0,0 +1,5 @@
+package com.black_dog20.servertabinfo.utility;
+
+public class PlayerInfo {
+
+}
