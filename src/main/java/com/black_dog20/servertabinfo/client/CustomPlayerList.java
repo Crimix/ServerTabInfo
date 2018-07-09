37f2c68cbf662ef2f4bf9de9dbcb2958df296468
Crimix <christofferm@live.dk>
2018-07-09 11:37:14 +0200

    - Made nicer formatting  - Creating pages for more elements that can be on the screen

--- src/main/java/com/black_dog20/servertabinfo/client/CustomPlayerList.java
+++ src/main/java/com/black_dog20/servertabinfo/client/CustomPlayerList.java
@@ -7,6 +7,7 @@ import java.util.List;
 
 import com.black_dog20.servertabinfo.client.objects.IRenderable;
 import com.black_dog20.servertabinfo.client.objects.Player;
+import com.black_dog20.servertabinfo.utility.CompatibilityHelper;
 import com.black_dog20.servertabinfo.utility.RenderHelper;
 import com.black_dog20.servertabinfo.utility.TpsDimension;
 import com.google.common.collect.ComparisonChain;
@@ -25,6 +26,10 @@ public class CustomPlayerList
     private static final Ordering<NetworkPlayerInfo> ENTRY_ORDERING = Ordering.from(new CustomPlayerList.PlayerComparator());
     private final Minecraft mc;
     public static HashMap<String, TpsDimension> playerDims = new HashMap<>();
+	private int maxPages = 1;
+	private int currentPage = 1;
+	private int ticks = 0;
+	private int changeTicks = 200;
 
     public CustomPlayerList(Minecraft mc)
     {
@@ -32,17 +37,38 @@ public class CustomPlayerList
     }
     
     public boolean render(int width) {
+    	ticks++;
     		int y = 10;
     		NetHandlerPlayClient nethandlerplayclient = this.mc.thePlayer.sendQueue;
     		List<NetworkPlayerInfo> list = ENTRY_ORDERING.<NetworkPlayerInfo>sortedCopy(nethandlerplayclient.getPlayerInfoMap());
     		List<IRenderable> playerList = new ArrayList<IRenderable>();
+    		int hith = (int) (GuiTabPage.hight-y-(GuiTabPage.hight*0.15));
+    		
+    		int itemPerPage = (int) Math.floor(hith/this.mc.fontRendererObj.FONT_HEIGHT/2);
+    		
     		for (NetworkPlayerInfo networkplayerinfo : list) {
     			playerList.add(new Player(networkplayerinfo,mc));
     		}
-        
-    		RenderHelper.RenderObjectList(playerList, mc, y, width);
+    		maxPages = (int)Math.ceil(playerList.size() / (double)itemPerPage);
+    		if(ticks%changeTicks == 0) {
+    			ticks=0;
+    			changePage();
+    		}
+    		int yy = RenderHelper.RenderObjectList(RenderHelper.getPage(currentPage,itemPerPage,playerList), mc, y, width);
+    		String s = "Page " + Integer.toString(currentPage) +" of " + Integer.toString(maxPages);
+    		int x = GuiTabPage.width / 2;
+    		CompatibilityHelper.drawStringWithShadow(mc, s, (float) x+2, (float) yy, -1);
     		return true;
     }  
+	
+	private void changePage() {
+		if(currentPage == maxPages) {
+			currentPage = 1;
+		}
+		else {
+			currentPage++;
+		}
+	}
 
     @SideOnly(Side.CLIENT)
     static class PlayerComparator implements Comparator<NetworkPlayerInfo>
